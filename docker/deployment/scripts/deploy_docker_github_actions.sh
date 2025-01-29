e3cc020588417f691e01b4212cbac7459262e467
brianlball <brian.ball@nrel.gov>
2025-01-29 08:19:02 -0600

    testup test branch for ARM64 build

--- docker/deployment/scripts/deploy_docker_github_actions.sh
+++ docker/deployment/scripts/deploy_docker_github_actions.sh
@@ -16,6 +16,8 @@ elif [ "${GITHUB_REF}" == "refs/heads/setup_github_actions" ]; then
     IMAGETAG=experimental
 #elif [ "${GITHUB_REF}" == "refs/heads/3.6.1-4" ]; then
 #     IMAGETAG="3.6.1-4"
+elif [ "${GITHUB_REF}" == "refs/heads/buildx-test" ]; then
+     IMAGETAG="3.9.0-test"
 fi
 
 if [ "${IMAGETAG}" != "skip" ]; then
